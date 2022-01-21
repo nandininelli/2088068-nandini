97ba43db404bf1e7724c6bbba2e0767277161b68
Nandini <2088068@cognizant.com>
2022-01-21 21:45:13 +0530

    moq handsoin submitted

---
+++ Moq/Handson 1/CustomerCommLib/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks; 
+using NUnit.Framework;
+using Moq;
+using CustomeCommLib;
+
+namespace CustomerCommLibTests
+{
+    [TestFixture]
+    public class CustomerCommLibTests{
+        [Test]
+        public void Test1()
+        {
+            Mock<IMailSender>moqob= new Mock<IMailSender>();
+            moqob.Setup(m => m.SendMail("customer234@abc.com","Message")).Returns(true);
+            CustomeCommLib.CustomeComm cust = new CustomeCommLib.CustomeComm(moqob.Object); 
+            bool result = cust.SendMailToCustomer();
+            Assert.AreEqual(true, result);
+        }
+    }
+}
+
---
+++ Moq/Handson 1/CustomerCommLib/MagicFilesLib/DirectoryExplorer.Tests/test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MagicFilesLib;
+
+using NUnit.Framework;
+
+
+using Moq;
+
+
+namespace DirectoryExplorer.Tests
+{
+    [TestFixture]
+    public class test
+    {
+        private readonly string _file1 = "file.txt";
+
+        private readonly string _file2 = "file2.txt";
+
+        private readonly ICollection<string> files = new List<string>();
+
+        Mock<IDirectoryExplorer> mockObj;
+        [OneTimeSetUp]
+
+public void init()
+
+        {
+
+            files.Add(_file1);
+
+            files.Add(_file2);
+
+            mockObj = new Mock<IDirectoryExplorer>();
+
+            mockObj.Setup(m => m.GetFiles(@"C:\")).Returns(files);
+
+        }
+
+        [Test]
+   public void Test_NotNull()
+        {
+            Assert.IsNotNull(mockObj.Object.GetFiles(@"C:\"));
+        }
+
+        [Test]
+        public void Test_CountEqual()
+        {
+            Assert.AreEqual(mockObj.Object.GetFiles(@"C:\").Count, 2);
+        }
+
+        [Test]
+        public void Test_ContainsFile1()
+        {
+            Assert.That(mockObj.Object.GetFiles(@"C:\").Contains(_file1));
+        }
+        
+    }
+}
