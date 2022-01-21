76f987f8aef4f9af882b1e8c1682394b71ddded3
Nandini <2088068@cognizant.com>
2022-01-21 10:46:54 +0530

    Handson for NUnit submitted

---
+++ Moq/Handson 3/PlayerManager/PlayerManager/IPlayerMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerManager
+{
+   public  interface IPlayerMapper
+    {
+        bool IsPlayerNameExistsInDb(string name);
+
+
+
+        void AddNewPlayerIntoDb(string name);
+    }
+}
---
+++ Moq/Handson 3/PlayerManager/PlayerManagerTests/PlayerMapperTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using PlayerManager;
+
+namespace PlayerManagerTests
+{
+    [TestFixture]
+    public class PlayerMapperTest
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            var moq = new Mock<IPlayerMapper>();
+            Player player = Player.RegisterNewPlayer("PlayerMapperTest", moq.Object);
+        }
+        [Test]
+        public void AlreadyExistsTest()
+        {
+            var moq = new Mock<IPlayerMapper>();
+            moq.Setup(x => x.IsPlayerNameExistsInDb(It.IsAny<string>())).Returns(false);
+            Player player = Player.RegisterNewPlayer("Test", moq.Object);
+            Assert.AreEqual("Test", player.Name);
+            Assert.AreEqual(23, player.Age);
+            Assert.AreEqual(30, player.NoOfMatches);
+        }
+    }
+}
+
